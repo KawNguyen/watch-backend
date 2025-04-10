a9062458a8b911cd4af4970383a7ad697f79d0b6
KawNguyen <khoanguyencool12@gmail.com>
2025-04-10 18:57:49 +0700

    update

--- src/config/cors.config.ts
+++ src/config/cors.config.ts
@@ -1,7 +1,7 @@
 import { CorsOptions } from 'cors';
 
 const corsOptions: CorsOptions = {
-  origin: true,  // Allow all origins during development
+  origin: 'http://localhost:5173',  // Allow all origins during development
   credentials: true,
   methods: ['GET', 'POST', 'PUT', 'PATCH', 'DELETE', 'OPTIONS'],
   allowedHeaders: [
