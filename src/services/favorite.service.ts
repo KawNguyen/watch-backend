779effdd21db8af04094255e6c59ca8777ae24c6
KawNguyen <khoanguyencool12@gmail.com>
2025-05-12 19:49:55 +0700

    update

--- src/services/favorite.service.ts
+++ src/services/favorite.service.ts
@@ -1,7 +1,6 @@
 import { PrismaClient } from "@prisma/client";
 
 const prisma = new PrismaClient();
-const DEFAULT_PAGE_SIZE = 20;
 
 export class FavoriteService {
   async addToFavorites(userId: string, watchId: string) {
